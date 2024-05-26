d7a294acdadfa7394794e765f227ee6703619e02
znkbnk <zenikibeniki@hotmail.com>
2024-05-26 22:25:53 +0100

    new dependencie

--- src/Exercises/solutions/CodeBlock.js
+++ src/Exercises/solutions/CodeBlock.js
@@ -1,13 +1,7 @@
 import React, { useState } from 'react';
-import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'; 
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import hljs from 'highlight.js';
+import Prism from 'prismjs';
+import 'prismjs/themes/prism.css';
 import "../../styles/solution.css";
-
-hljs.configure({
-  languages: ['javascript'],
-});
 
 const CodeBlock = ({ code }) => {
   const [copied, setCopied] = useState(false);
@@ -20,14 +14,16 @@ const CodeBlock = ({ code }) => {
   return (
     <div className="solution-container">
       <div className="code-block">
-        <SyntaxHighlighter language="javascript" style={a11yDark} className="custom-code-block">
-          {code}
-        </SyntaxHighlighter>
-        <CopyToClipboard text={code} onCopy={handleCopy}>
-          <div className="copy-button">
-            <button className="button-84">{copied ? "Copied!" : "Copy Code"}</button>
-          </div>
-        </CopyToClipboard>
+        <pre className="custom-code-block">
+          <code className="language-javascript">
+            {code}
+          </code>
+        </pre>
+        <div className="copy-button">
+          <button className="button-84" onClick={handleCopy}>
+            {copied ? "Copied!" : "Copy Code"}
+          </button>
+        </div>
       </div>
     </div>
   );
