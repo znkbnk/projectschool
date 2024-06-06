bc4dc4b78a1cec878df94acc371145c5466c66a4
znkbnk <zenikibeniki@hotmail.com>
2024-06-06 23:08:23 +0100

    d

--- netlify/functions/stripe-webhook.js
+++ netlify/functions/stripe-webhook.js
@@ -1,6 +1,11 @@
 const admin = require('firebase-admin');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
+// Initialize Firebase Admin SDK
+if (!admin.apps.length) { // Check if the app has already been initialized
+    admin.initializeApp();
+  }
+
 async function updateFirebaseStatus(customerId) {
   try {
     const db = admin.firestore();
@@ -19,29 +24,29 @@ async function updateFirebaseStatus(customerId) {
 }
 
 exports.handler = async (event) => {
-    try {
-      const sig = event.headers['stripe-signature'];
-      const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
-      const stripeEvent = stripe.webhooks.constructEvent(event.body, sig, webhookSecret);
+  try {
+    const sig = event.headers['stripe-signature'];
+    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+    const stripeEvent = stripe.webhooks.constructEvent(event.body, sig, webhookSecret);
   
-      // Handle specific event types, e.g., checkout.session.completed
-      if (stripeEvent.type === 'checkout.session.completed') {
-        const session = stripeEvent.data.object;
-        const customerId = session.customer;
+    // Handle specific event types, e.g., checkout.session.completed
+    if (stripeEvent.type === 'checkout.session.completed') {
+      const session = stripeEvent.data.object;
+      const customerId = session.customer;
   
-        // Update user status in Firebase database
-        await updateFirebaseStatus(customerId);
-      }
-  
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ received: true }),
-      };
-    } catch (error) {
-      console.error('Error handling webhook:', error);
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: 'Webhook handling failed' }),
-      };
+      // Update user status in Firebase database
+      await updateFirebaseStatus(customerId);
     }
-  };
+  
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ received: true }),
+    };
+  } catch (error) {
+    console.error('Error handling webhook:', error);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'Webhook handling failed' }),
+    };
+  }
+};
