aead48d5db59254de99bd75e3309068047e8afcc
znkbnk <zenikibeniki@hotmail.com>
2024-06-06 20:18:03 +0100

    g

--- netlify/functions/stripe-webhook.js
+++ netlify/functions/stripe-webhook.js
@@ -1,11 +1,25 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const admin = require('firebase-admin');
-const serviceAccount = require('../../projectschool-48842.json');
+
 const sgMail = require('@sendgrid/mail'); 
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+const serviceAccountData = {
+     type: 'service_account',
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-m69uf%40projectschool-48842.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+  };
+  
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccountData)
+  });
 
 exports.handler = async (event, context) => {
   const sig = event.headers['stripe-signature'];
