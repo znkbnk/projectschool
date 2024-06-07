c996baa90dfcbd4648b47111824beee806ffd3ce
znkbnk <zenikibeniki@hotmail.com>
2024-06-07 21:39:17 +0100

    d

--- netlify/functions/stripe-webhook.js
+++ netlify/functions/stripe-webhook.js
@@ -16,9 +16,9 @@ const serviceAccount = {
   "client_email": process.env.FIREBASE_CLIENT_EMAIL,
   "client_id": process.env.FIREBASE_CLIENT_ID,
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://accounts.google.com/o/oauth2/token",
+  "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-   "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+   "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
 };
 
 // Initialize Firebase Admin SDK
