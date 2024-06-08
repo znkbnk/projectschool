210f1c4c795305f8deb8c0b4d923647b2badf2a9
znkbnk <zenikibeniki@hotmail.com>
2024-06-08 22:38:59 +0100

    a

---
+++ src/Stripe/Checkout.js
@@ -0,0 +1,20 @@
+// import { useState } from "react";
+// import { loadStripe } from "@stripe/stripe-js";
+import "../styles/checkout.css";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import PriceCardsList from "./PriceCardsList";
+import CheckoutTitle from "./CheckoutTitle";
+
+const Checkout = () => {
+  return (
+    <div>
+      <Navbar />
+      <CheckoutTitle />
+      <PriceCardsList  />
+      <Footer />
+    </div>
+  );
+};
+
+export default Checkout;
