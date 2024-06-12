041fd0bc4d5348c8da19ca066c10bc716bff8df5
znkbnk <zenikibeniki@hotmail.com>
2024-06-12 19:34:23 +0100

    a

--- netlify/functions/create-user.js
+++ netlify/functions/create-user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const User = require('../backend/models/userModel'); 
+const User = require('../../backend/models/userModel'); 
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
