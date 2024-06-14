a7561e3a65d049c0d2a6589fab56acb4c23fe89d
znkbnk <zenikibeniki@hotmail.com>
2024-06-14 16:29:09 +0100

    a

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './netlify/functions/create-user.js',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist/functions'),
+    filename: 'create-user.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+};
