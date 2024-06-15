3b5d3810d5cd07294dd2816c0c1c2ed53d84463b
znkbnk <zenikibeniki@hotmail.com>
2024-06-15 18:51:02 +0100

    a

--- src/Stripe/Success.js
+++ src/Stripe/Success.js
@@ -1,31 +1,24 @@
-import React, {  useState } from "react";
+// SuccessComponent.js
+
+import React from "react";
+import "../styles/success.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const Success = () => {
-  const [error, setError] = useState(null);
-
-
   return (
     <div>
       <Navbar />
-      <div className="success-container">
-        <div className="success-card">
-          <div className="icon-container">
-            <i className="checkmark">✓</i>
+      <div className='success-container'>
+        <div className='success-card'>
+          <div className='icon-container'>
+            <i className='checkmark'>✓</i>
           </div>
           <h1>Success</h1>
-          {error ? (
-            <p className="error-text">An error occurred: {error}</p>
-          ) : (
-            <>
-              <p className="success-text">
-                We received your purchase request;
-                <br /> we'll be in touch shortly!
-              </p>
-              
-            </>
-          )}
+          <p className='success-text'>
+            We received your purchase request;
+            <br /> we'll be in touch shortly!
+          </p>
         </div>
       </div>
       <Footer />
