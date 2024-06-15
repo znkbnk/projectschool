9007aa0a7d6d982488b28f3ce5408a28ac7db741
znkbnk <zenikibeniki@hotmail.com>
2024-06-15 20:22:53 +0100

    a

--- src/Stripe/Success.js
+++ src/Stripe/Success.js
@@ -1,11 +1,40 @@
-// SuccessComponent.js
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/success.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { auth } from "firebase-admin";
+import axios from "axios";
 
 const Success = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        handleSubscriptionUpdate(currentUser.uid);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const handleSubscriptionUpdate = async (firebaseUid) => {
+    try {
+      await axios.post(
+        "https://projectschool404-4c33494b2162.herokuapp.com/api/update-subscription",
+        { firebaseUid }
+      );
+
+      console.log("Subscription status updated successfully");
+    } catch (error) {
+      console.error("Error updating subscription status:", error);
+    }
+  };
+
   return (
     <div>
       <Navbar />
