0784d84adaeee042c9ff65b07b0cce9e44690def
znkbnk <zenikibeniki@hotmail.com>
2024-06-22 22:42:56 +0100

    live toast message changed

--- src/Exercises/LiveEditor.js
+++ src/Exercises/LiveEditor.js
@@ -160,7 +160,9 @@ const LiveEditor = () => {
     if (subscriptionStatus === 'subscribed') {
       setShowSolution(!showSolution);
     } else {
-      toast.info('Access to solutions requires an active subscription. Please subscribe to unlock this feature.');
+      // toast.info('Access to solutions requires an active subscription. Please subscribe to unlock this feature.');
+
+      toast.info('Solution not available at the moment');
 
     }
   };
