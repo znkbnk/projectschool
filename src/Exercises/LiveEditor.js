d8d9c82842652bd5dc7c538ce6c3eabff069a2e9
znkbnk <zenikibeniki@hotmail.com>
2024-08-21 22:26:40 +0100

    task 55 added

--- src/Exercises/LiveEditor.js
+++ src/Exercises/LiveEditor.js
@@ -6,8 +6,8 @@ import Navbar from "../components/Navbar";
 import "../styles/editor.css";
 import "react-toastify/dist/ReactToastify.css";
 import CodeBlock from "./solutions/CodeBlock";
-import { auth } from '../components/firebase';
-import axios from 'axios';
+import { auth } from "../components/firebase";
+import axios from "axios";
 
 const LiveEditor = () => {
   const { lessonType, taskId } = useParams();
@@ -25,58 +25,63 @@ const LiveEditor = () => {
         const user = auth.currentUser;
         if (user) {
           const { uid } = user;
-          const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/users/${uid}/subscription-status`);
-          console.log('Subscription status:', response.data);
+          const response = await axios.get(
+            `${process.env.REACT_APP_API_URL}/api/users/${uid}/subscription-status`
+          );
+          console.log("Subscription status:", response.data);
           setSubscriptionStatus(response.data.subscriptionStatus);
         }
       } catch (error) {
-        console.error('Error fetching subscription status:', error);
-        toast.error('Error fetching subscription status');
+        console.error("Error fetching subscription status:", error);
+        toast.error("Error fetching subscription status");
       }
     };
-  
+
     fetchSubscriptionStatus();
   }, []);
 
   useEffect(() => {
     const lessonCompletedTasksKey = `${lessonType}_completedTasks`;
-    const completedTasks = JSON.parse(localStorage.getItem(lessonCompletedTasksKey)) || {};
+    const completedTasks =
+      JSON.parse(localStorage.getItem(lessonCompletedTasksKey)) || {};
     setIsCompleted(completedTasks[taskId] || false);
   }, [lessonType, taskId]);
 
   useEffect(() => {
     if (lessonType && taskId) {
-      const index = tasksData[lessonType].findIndex(task => task.taskId === taskId);
-      setCurrentTaskIndex(index);
-      const storedCheckboxStates = JSON.parse(localStorage.getItem(taskId)) || {};
-      setCheckboxStates(storedCheckboxStates);
+      const index = tasksData[lessonType]?.findIndex(
+        (task) => task.taskId === taskId
+      );
+      if (index !== undefined && index >= 0) {
+        setCurrentTaskIndex(index);
+        const storedCheckboxStates =
+          JSON.parse(localStorage.getItem(taskId)) || {};
+        setCheckboxStates(storedCheckboxStates);
+      } else {
+        console.error(`Task with ID ${taskId} not found.`);
+      }
     }
   }, [lessonType, taskId]);
 
   useEffect(() => {
     import(`./solutions/${taskId}`)
-      .then(module => {
+      .then((module) => {
         if (Array.isArray(module.default)) {
           setSolutionCodes(module.default);
         } else {
           setSolutionCodes([module.default]);
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error loading solution:", error);
-        // Handle the error here, potentially display a message to the user
         setSolutionCodes(["Solution not found"]);
       });
   }, [taskId]);
 
-
-
-  
-
-  const handleCheckboxChange = stepId => {
-    setCheckboxStates(prevState => ({
+  const handleCheckboxChange = (stepId) => {
+    setCheckboxStates((prevState) => ({
       ...prevState,
-      [stepId]: !prevState[stepId]
+      [stepId]: !prevState[stepId],
     }));
   };
 
@@ -85,7 +90,10 @@ const LiveEditor = () => {
   }, [taskId, checkboxStates]);
 
   const handleNext = () => {
-    if (lessonType && currentTaskIndex < tasksData[lessonType].length - 1) {
+    if (
+      lessonType &&
+      currentTaskIndex < (tasksData[lessonType]?.length || 0) - 1
+    ) {
       if (!isCompleted) {
         toast.warn("Please complete the current task first.");
       } else {
@@ -110,16 +118,22 @@ const LiveEditor = () => {
       return;
     }
 
-    const taskIndex = tasksData[lessonType].findIndex(task => task.taskId === taskId);
+    const taskIndex = tasksData[lessonType]?.findIndex(
+      (task) => task.taskId === taskId
+    );
 
     if (taskIndex !== -1) {
       const lessonCompletedTasksKey = `${lessonType}_completedTasks`;
       const updatedTasksData = { ...tasksData };
       updatedTasksData[lessonType][taskIndex].completed = true;
 
-      const completedTasks = JSON.parse(localStorage.getItem(lessonCompletedTasksKey)) || {};
+      const completedTasks =
+        JSON.parse(localStorage.getItem(lessonCompletedTasksKey)) || {};
       completedTasks[taskId] = true;
-      localStorage.setItem(lessonCompletedTasksKey, JSON.stringify(completedTasks));
+      localStorage.setItem(
+        lessonCompletedTasksKey,
+        JSON.stringify(completedTasks)
+      );
 
       console.log(`Task ${taskId} marked as completed.`);
       toast.success(`Lesson ${lessonType} is completed!`);
@@ -130,7 +144,7 @@ const LiveEditor = () => {
   };
 
   const handleDownloadStyles = () => {
-    const styleLink = tasksData[lessonType][currentTaskIndex].link;
+    const styleLink = currentTask?.link;
     if (styleLink) {
       window.open(styleLink, "_blank");
     } else {
@@ -139,7 +153,7 @@ const LiveEditor = () => {
   };
 
   const handleDownloadImg = () => {
-    const imgLink = tasksData[lessonType][currentTaskIndex].linkImg;
+    const imgLink = currentTask?.linkImg;
     if (imgLink) {
       window.open(imgLink, "_blank");
     } else {
@@ -148,7 +162,7 @@ const LiveEditor = () => {
   };
 
   const handleDownloadData = () => {
-    const dataLink = tasksData[lessonType][currentTaskIndex].linkData;
+    const dataLink = currentTask?.linkData;
     if (dataLink) {
       window.open(dataLink, "_blank");
     } else {
@@ -157,18 +171,17 @@ const LiveEditor = () => {
   };
 
   const handleToggleSolution = () => {
-    if (subscriptionStatus === 'subscribed') {
+    if (subscriptionStatus === "subscribed") {
       setShowSolution(!showSolution);
     } else {
-      toast.info('Access to solutions requires an active subscription. Please subscribe to unlock this feature.');
-
-      // toast.info('Solution not available at the moment');
-
+      toast.info(
+        "Access to solutions requires an active subscription. Please subscribe to unlock this feature."
+      );
     }
   };
 
-  const currentTask = tasksData[lessonType][currentTaskIndex];
-  const codesandboxUrl = currentTask ? currentTask.codesandboxUrl : "";
+  const currentTask = tasksData[lessonType]?.[currentTaskIndex];
+  const codesandboxUrl = currentTask?.codesandboxUrl || "";
 
   return (
     <div>
@@ -177,107 +190,112 @@ const LiveEditor = () => {
         <div className='task-container'>
           <div className='task'>
             <div className='text-window'>
-              <h1>{currentTask.taskTitle}</h1>
+              <h1>{currentTask?.taskTitle}</h1>
               {lessonType &&
-                currentTask.steps.map((step, index) => (
+                currentTask?.steps?.map((step, index) => (
                   <div className='taskText-container' key={index}>
-                    <h3>{step.stepTitle}</h3>
-                    <h4>{step.titleDescription}</h4>
-                    <div className='checkbox-container'>
-                      <input
-                        type='checkbox'
-                        id={`step${index}`}
-                        checked={checkboxStates[`step${index}`]}
-                        onChange={() => handleCheckboxChange(`step${index}`)}
-                      />
-                      <label htmlFor={`step${index}`}>
-                        <ul>
-                          {step.description.map((desc, descIndex) => (
+                    <div className='step-title-container'>
+                      <div className='checkbox-container'>
+                        <input
+                          type='checkbox'
+                          id={`step${index}`}
+                          checked={checkboxStates[`step${index}`] || false}
+                          onChange={() => handleCheckboxChange(`step${index}`)}
+                        />
+                        <label htmlFor={`step${index}`}></label>
+                      </div>
+                      <h3>{step.stepTitle}</h3>
+                    </div>
+
+                    <p>{step.titleDescription}</p>
+                    {step.sections?.map((section, secIndex) => (
+                      <div className='section' key={secIndex}>
+                        <h4>{section.subtitleDescription}</h4>
+                        <ul className='text-window-list'>
+                          {section.descriptions?.map((desc, descIndex) => (
                             <li key={descIndex}>{desc}</li>
                           ))}
                         </ul>
-                      </label>
-                    </div>
+                      </div>
+                    ))}
+
                     <br />
                   </div>
                 ))}
               <div className='task-button-container'>
-                {currentTask.link && (
+                {currentTask?.link && (
                   <button className='button-84' onClick={handleDownloadStyles}>
                     Download Styles
                   </button>
                 )}
-                {currentTask.linkImg && (
+                {currentTask?.linkImg && (
                   <button className='button-84' onClick={handleDownloadImg}>
                     Download Images
                   </button>
-                )}{" "}
-                {currentTask.linkData && (
-                 
-                 <button className='button-84' onClick={handleDownloadData}>
-                 Download Data
-               </button>
-             )}
-                
-                  <button className='button-84' onClick={handleToggleSolution}>
-                    Solution
+                )}
+                {currentTask?.linkData && (
+                  <button className='button-84' onClick={handleDownloadData}>
+                    Download Data
                   </button>
-              
-           </div>
-           {showSolution && (
-             <div className='solution-popup'>
-               <div className='solution-container'>
-                 <h2 className='solution-title'>
-                   Solution code for:{" "}
-                   {currentTask ? currentTask.taskTitle : "Task Title"}
-                 </h2>
-                 {solutionCodes.map((code, index) => (
-                   <CodeBlock
-                     key={index}
-                     code={code}
-                     className='code-block'
-                   />
-                 ))}
-                 <button
-                   className='close-button button-84'
-                   onClick={handleToggleSolution}
-                 >
-                   Close
-                 </button>
-               </div>
-             </div>
-           )}
-         </div>
-       </div>
-       {lessonType && lessonType.length > 0 && (
-         <div className='task-buttons'>
-           <button className='button-28 previous' onClick={handlePrevious}>
-             Previous
-           </button>
-           <button
-             className={`button-28 complete ${
-               isCompleted ? "completed" : ""
-             }`}
-             onClick={handleComplete}
-           >
-             {isCompleted ? "Completed" : "Complete"}
-           </button>
-           <button className='button-28 next' onClick={handleNext}>
-             Next
-           </button>
-         </div>
-       )}
-     </div>
-     <iframe
-       src={codesandboxUrl}
-       title='React'
-       allow='accelerometer; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb;'
-       sandbox='allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts'
-     ></iframe>
-   </div>
-   <ToastContainer />
- </div>
-);
+                )}
+                <button className='button-84' onClick={handleToggleSolution}>
+                  Solution
+                </button>
+              </div>
+              {showSolution && (
+                <div className='solution-popup'>
+                  <div className='solution-container'>
+                    <h2 className='solution-title'>
+                      Solution code for:{" "}
+                      {currentTask?.taskTitle || "Task Title"}
+                    </h2>
+                    {solutionCodes.map((code, index) => (
+                      <CodeBlock
+                        key={index}
+                        code={code}
+                        className='code-block'
+                      />
+                    ))}
+                    <button
+                      className='close-button button-84'
+                      onClick={handleToggleSolution}
+                    >
+                      Close
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+          {lessonType && lessonType.length > 0 && (
+            <div className='task-buttons'>
+              <button className='button-28 previous' onClick={handlePrevious}>
+                Previous
+              </button>
+              <button
+                className={`button-28 complete ${
+                  isCompleted ? "completed" : ""
+                }`}
+                onClick={handleComplete}
+              >
+                {isCompleted ? "Completed" : "Complete"}
+              </button>
+              <button className='button-28 next' onClick={handleNext}>
+                Next
+              </button>
+            </div>
+          )}
+        </div>
+        <iframe
+          src={codesandboxUrl}
+          title='React'
+          allow='accelerometer; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb;'
+          sandbox='allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts'
+        ></iframe>
+      </div>
+      <ToastContainer />
+    </div>
+  );
 };
 
 export default LiveEditor;
---
+++ src/data/array2.js
@@ -0,0 +1,132 @@
+steps: [
+    {
+       stepTitle: "Step 1: ",
+       sections: [
+         {
+           descriptions: [
+             "Detail 1.1",
+             "Detail 1.2",
+             "Detail 1.3"
+           ],
+         },
+        
+       ],
+     },
+      {
+       stepTitle: "Step 2: ",
+       sections: [
+         {
+           descriptions: [
+             "Detail 1.1",
+             "Detail 1.2",
+             "Detail 1.3"
+           ],
+         },
+       
+       ],
+     },
+      {
+       stepTitle: "Step 3: ",
+       sections: [
+         {
+           descriptions: [
+             "Detail 1.1",
+             "Detail 1.2",
+             "Detail 1.3"
+           ],
+         },
+      
+       ],
+     },
+      {
+       stepTitle: "Step 4: ",
+       sections: [
+         {
+           descriptions: [
+             "Detail 1.1",
+             "Detail 1.2",
+             "Detail 1.3"
+           ],
+         },
+       
+       ],
+     },
+      {
+       stepTitle: "Step 5: ",
+       sections: [
+         {
+           descriptions: [
+             "Detail 1.1",
+             "Detail 1.2",
+             "Detail 1.3"
+           ],
+         },
+       
+       ],
+     },
+      {
+       stepTitle: "Step 6: ",
+       sections: [
+         {
+           descriptions: [
+             "Detail 1.1",
+             "Detail 1.2",
+             "Detail 1.3"
+           ],
+         },
+        
+       ],
+     },
+      {
+       stepTitle: "Step 7: ",
+       sections: [
+         {
+           descriptions: [
+             "Detail 1.1",
+             "Detail 1.2",
+             "Detail 1.3"
+           ],
+         },
+        
+       ],
+     },
+      {
+       stepTitle: "Step 8: ",
+       sections: [
+         {
+           descriptions: [
+             "Detail 1.1",
+             "Detail 1.2",
+             "Detail 1.3"
+           ],
+         },
+        
+       ],
+     },
+      {
+       stepTitle: "Step 9: ",
+       sections: [
+         {
+           descriptions: [
+             "Detail 1.1",
+             "Detail 1.2",
+             "Detail 1.3"
+           ],
+         },
+         
+       ],
+     },
+      {
+       stepTitle: "Step 10: ",
+       sections: [
+         {
+           descriptions: [
+             "Detail 1.1",
+             "Detail 1.2",
+             "Detail 1.3"
+           ],
+         },
+        
+       ],
+     },
+   ],
